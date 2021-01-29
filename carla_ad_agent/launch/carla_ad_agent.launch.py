ef6ad9a2138d4952dbc0e0931f32604b1585e134
jmagnin <jean-baptiste.magnin@intel.com>
2021-01-29 11:40:13 +0100

    clean up carla_ad_agent

--- carla_ad_agent/launch/carla_ad_agent.launch.py
+++ carla_ad_agent/launch/carla_ad_agent.launch.py
@@ -4,69 +4,54 @@ import sys
 import launch
 import launch_ros.actions
 
-
-# def launch_setup(context, *args, **kwargs):
-#     # workaround to use launch argument 'role_name' as a part of the string used for the node's name
-#     node_name = 'carla_ad_agent_' + \
-#         launch.substitutions.LaunchConfiguration('role_name').perform(context)
-
-#     carla_ad_demo = launch_ros.actions.Node(
-#         package='carla_ad_agent',
-#         executable='carla_ad_agent',
-#         name=node_name,
-#         output='screen',
-#         parameters=[
-#             {
-#                 'target_speed': launch.substitutions.LaunchConfiguration('target_speed')
-#             },
-#             {
-#                 'role_name': launch.substitutions.LaunchConfiguration('role_name')
-#             },
-#             {
-#                 'avoid_risk': launch.substitutions.LaunchConfiguration('avoid_risk')
-#             }
-#         ]
-#     )
-
-#     return [carla_ad_demo]
-
-
 def generate_launch_description():
     ld = launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(
             name='role_name',
             default_value='ego_vehicle'
         ),
-        launch.actions.SetLaunchConfiguration(
-            name='test',
-            value='jb'
-        ),
         launch.actions.DeclareLaunchArgument(
             name='target_speed',
-            default_value='30'
+            default_value='30.0'
         ),
         launch.actions.DeclareLaunchArgument(
             name='avoid_risk',
             default_value='True'
         ),
-        # launch.actions.OpaqueFunction(function=launch_setup),
+        launch.actions.DeclareLaunchArgument(
+            name='Kp_lateral',
+            default_value='0.9'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='Ki_lateral',
+            default_value='0.0'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='Kd_lateral',
+            default_value='0.0'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='Kp_longitudinal',
+            default_value='0.206'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='Ki_longitudinal',
+            default_value='0.0206'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='Kd_longitudinal',
+            default_value='0.515'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='control_time_step',
+            default_value='0.05'
+        ),
         launch_ros.actions.Node(
             package='carla_ad_agent',
-            executable='local_planner',
-            name=['local_planner_', launch.substitutions.LaunchConfiguration('role_name')],
-            remappings=[
-                (
-                    ["/carla/",
-                        launch.substitutions.LaunchConfiguration('role_name'), "/target_speed"],
-                    ["/carla/",
-                        launch.substitutions.LaunchConfiguration('role_name'), "/target_speed_to_pid"]
-                )
-            ],
+            executable='carla_ad_agent',
+            name=['carla_ad_agent_', launch.substitutions.LaunchConfiguration('role_name')],
             output='screen',
             parameters=[
-                {
-                    'use_sim_time': True
-                },
                 {
                     'target_speed': launch.substitutions.LaunchConfiguration('target_speed')
                 },
@@ -75,6 +60,20 @@ def generate_launch_description():
                 },
                 {
                     'avoid_risk': launch.substitutions.LaunchConfiguration('avoid_risk')
+                }
+            ]
+        ),
+        launch_ros.actions.Node(
+            package='carla_ad_agent',
+            executable='local_planner',
+            name=['local_planner_', launch.substitutions.LaunchConfiguration('role_name')],
+            output='screen',
+            parameters=[
+                {
+                    'use_sim_time': True
+                },
+                {
+                    'role_name': launch.substitutions.LaunchConfiguration('role_name')
                 }
             ]
         )
